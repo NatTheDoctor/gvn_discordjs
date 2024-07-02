e48faa1d4cb8d155576cf24f3ab395a389b9e654
NatTheDoctor <quan9799.jp@gmail.com>
2024-07-02 23:39:40 +0700

    b

--- src/events/Client/onMessage.js
+++ src/events/Client/onMessage.js
@@ -1,11 +1,12 @@
 const { success } = require("../../utils/Console");
 const Event = require("../../structure/Event");
-//const { fetchOrCreateMessage } = require("../../queries/messageQuery");
+const { fetchOrCreateMessage } = require("../../queries/messageQuery");
 
 module.exports = new Event({
   event: "messageCreate",
   once: false,
   run: (__client__, message) => {
-    //fetchOrCreateMessage(message);
+    console.log(message);
+    fetchOrCreateMessage(message);
   },
 }).toJSON();
