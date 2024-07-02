96e6cfdff267c269854dcf96725fc91ed23b3ff4
NatTheDoctor <quan9799.jp@gmail.com>
2024-07-02 20:10:14 +0700

    aba

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const client = new DiscordBot();
 module.exports = client;
 
 client.connect();
-connect("mongodb://nat:123@192.168.4.70:27017/nat");
+connect("mongodb://nat:24021999@192.168.4.70:27017/nat", {
+  connectTimeoutMS: 60000,
+}).catch(console.error);
 process.on("unhandledRejection", console.error);
 process.on("uncaughtException", console.error);
