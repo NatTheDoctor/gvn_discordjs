5e1c984ba6c7d409b1e6969bf1f1f661a18a6f60
NatTheDoctor <quan9799.jp@gmail.com>
2024-07-02 23:42:33 +0700

    ab

--- src/queries/messageQuery.js
+++ src/queries/messageQuery.js
@@ -1,13 +1,13 @@
 const MessageSchema = require("../schema/message");
 
-const fetchOrCreateMessage = async (message) => {
-  const message = await MessageSchema.findOne({ messageId: message.id }).lean();
+const fetchOrCreateMessage = async (m) => {
+  const message = await MessageSchema.findOne({ messageId: m.id }).lean();
   if (message) return message;
   const query = new MessageSchema({
-    messageId: message.id,
-    channelId: message.channel.id,
-    userId: message.author.id,
-    timestamp: message.createdAt,
+    messageId: m.id,
+    channelId: m.channel.id,
+    userId: m.author.id,
+    timestamp: m.createdAt,
   });
   await query.save();
   return query;
