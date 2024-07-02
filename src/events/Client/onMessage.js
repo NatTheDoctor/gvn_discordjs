b58767a09cba8f264948d5fdee0f6a4cf9c5f4a0
NatTheDoctor <quan9799.jp@gmail.com>
2024-07-03 02:58:08 +0700

    a

--- src/events/Client/onMessage.js
+++ src/events/Client/onMessage.js
@@ -18,6 +18,7 @@ module.exports = new Event({
   event: "messageCreate",
   once: false,
   run: async (__client__, message) => {
+    const startTime = performance.now();
     const author = message.author;
     const id = author.id;
     const guild = message.guild;
@@ -37,8 +38,10 @@ module.exports = new Event({
     } else {
       success("true");
     }
+    const endTime = performance.now();
+    const executionTime = endTime - startTime;
     success(
-      `${message.channel.name} ${user.userName}: coin ${user.coin}, exp ${user.exp}`
+      `${message.channel.name} ${user.userName}: coin ${user.coin}, exp ${user.exp} ${executionTime} ms`
     );
   },
 }).toJSON();
