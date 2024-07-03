5f34b4c855a868577e83f9272cf8a86620de7a34
NatTheDoctor <quan9799.jp@gmail.com>
2024-07-03 15:15:21 +0700

    ab

--- src/utils/anvas/anvaProcess.js
+++ src/utils/anvas/anvaProcess.js
@@ -40,4 +40,4 @@ const case1 = (author, target, amount) => {
   return `<@${target.userId}> ăn đấm, ngất tại chỗ, mất tiền\n\`${author.userName}\`: **+${amount}** ${iconCoin}\n\`${target.userName}\`: **-${amount}** ${iconCoin}`;
 };
 
-module.exports = anvaProcess;
+module.exports = { anvaProcess, case1 };
