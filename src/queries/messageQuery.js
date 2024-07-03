232abae577281051e496a0fa4d97e98f8a6b8255
NatTheDoctor <quan9799.jp@gmail.com>
2024-07-03 19:19:14 +0700

    ab

--- src/queries/messageQuery.js
+++ src/queries/messageQuery.js
@@ -34,6 +34,7 @@ const getAllMessage = async (interaction) => {
       { "status.isCaptive": { $ne: true } },
     ],
   });
+  console.log(members);
   let members = [];
   for (x of messages) {
     if (members.includes(x.userId)) continue;
