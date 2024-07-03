058c4ff0ce10af2a50b3a5e24483a9d83ba3a3e8
NatTheDoctor <quan9799.jp@gmail.com>
2024-07-03 15:19:25 +0700

    ab

--- src/commands/Utility/profile.js
+++ src/commands/Utility/profile.js
@@ -27,8 +27,6 @@ module.exports = new ApplicationCommand({
     }
     let profile = await fetchUser(interaction.user.id);
 
-    let result = anvaProcess();
-
     //embed.setDescription(result);
     await interaction.reply({ embeds: [embed], ephemeral: true });
   },
