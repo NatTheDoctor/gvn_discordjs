9e63660f24f9d61ef087b894dd1785e71522bff5
NatTheDoctor <quan9799.jp@gmail.com>
2024-07-03 11:15:47 +0700

    ab

--- src/events/Client/onUserUpdate.js
+++ src/events/Client/onUserUpdate.js
@@ -1,12 +1,14 @@
 const { success } = require("../../utils/Console");
 const Event = require("../../structure/Event");
 const { getMember } = require("../../utils/getMember");
+const { fetchUser } = require("../../queries/userQuery");
 module.exports = new Event({
   event: "guildMemberUpdate",
   once: false,
   run: async (__client__, message) => {
     const { author, id, guild, member } = await getMember(message);
-    console.log("a");
-    console.log(member);
+    let profile = await fetchUser(id);
+    if (profile === null) return;
+    success(profile);
   },
 }).toJSON();
--- src/utils/getMember.js
+++ src/utils/getMember.js
@@ -1,5 +1,4 @@
 async function getMember(message) {
-  console.log(message);
   const id = message.id;
   const guild = message.guild; // or message.guild, depending on your setup
   const member = await guild.members.fetch(id);
