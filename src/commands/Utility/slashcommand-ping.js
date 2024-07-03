b272839f1b6752b894247b5111d07fa0ab4de80f
NatTheDoctor <quan9799.jp@gmail.com>
2024-07-03 17:04:50 +0700

    ab

--- src/commands/Utility/slashcommand-ping.js
+++ src/commands/Utility/slashcommand-ping.js
@@ -27,6 +27,7 @@ module.exports = new ApplicationCommand({
   run: async (client, interaction) => {
     let profile = await fetchUser(interaction.user.id);
     if (profile === null) return;
+    if (interaction.user.id !== "475667108223647744") return;
     await setDebuff(profile.userId, StatusField.BAKIEN, true);
     await interaction.reply({
       content: "**Pong!** " + client.ws.ping + "ms",
