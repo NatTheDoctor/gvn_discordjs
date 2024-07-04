af405ea2953c558e32bc241097db1a88e786e421
NatTheDoctor <quan9799.jp@gmail.com>
2024-07-04 11:58:28 +0700

    ab

--- src/utils/scheduleDaily.js
+++ src/utils/scheduleDaily.js
@@ -1,13 +1,15 @@
 const cron = require("node-cron");
 const { setDaily, fetchAllDailyUser } = require("../queries/userQuery");
 
-const scheduleDaily = cron.schedule("* * * * *", async function () {
+const scheduleDaily = cron.schedule("* */12 * * *", async function () {
   let members = await fetchAllDailyUser();
   for (x in members) {
     if (members[x].isDaily) {
       await setDaily(members[x].userId, false);
     }
   }
+
+  success("Đã update daily");
 });
 
 module.exports = { scheduleDaily };
