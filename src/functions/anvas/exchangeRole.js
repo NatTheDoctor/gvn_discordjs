190d4c7efa46ed9909a9051c8a2d36ce0ca4df53
NatTheDoctor <quan9799.jp@gmail.com>
2024-07-05 06:50:55 +0700

    ab

--- src/functions/anvas/exchangeRole.js
+++ src/functions/anvas/exchangeRole.js
@@ -3,7 +3,7 @@ const { setBuff } = require("../../queries/userQuery");
 const exchangeBaKien = async (hadBaKien, notHaveBaKien) => {
   if (hadBaKien.isBaKien) {
     await setBuff(hadBaKien.userId, false);
-    await setBuff(notHaveBaKien.userId, false);
+    await setBuff(notHaveBaKien.userId, true);
   }
 };
 module.exports = { exchangeBaKien };
