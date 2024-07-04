8291b2a2d47e82afebed1f3ddf12e68509e42e2b
NatTheDoctor <quan9799.jp@gmail.com>
2024-07-04 10:46:19 +0700

    ab

--- src/events/Client/onButtonClick.js
+++ src/events/Client/onButtonClick.js
@@ -4,11 +4,21 @@ const Event = require("../../structure/Event");
 module.exports = new Event({
   event: "interactionCreate",
   once: false,
-  run: (__client__, interaction) => {
+  run: async (__client__, interaction) => {
     if (interaction.isButton()) {
       let customId = interaction.component.data.custom_id;
       if (customId === "accept") {
-        interaction.update("Đã chuyển khoản");
+        let embed = new EmbedBuilder().setDescription(`Đã chuyển khoản`);
+        await interaction.update({
+          embeds: [embed],
+          ephemeral: true,
+        });
+      } else if (customId === "deny") {
+        let embed = new EmbedBuilder().setDescription(`OK`);
+        await interaction.update({
+          embeds: [embed],
+          ephemeral: true,
+        });
       }
     }
   },
