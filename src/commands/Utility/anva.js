c27ca9d01b13b79f1c0e977c60097dae9679a321
NatTheDoctor <quan9799.jp@gmail.com>
2024-07-05 15:04:50 +0700

    ab

--- src/commands/Utility/anva.js
+++ src/commands/Utility/anva.js
@@ -24,50 +24,54 @@ module.exports = new ApplicationCommand({
     cooldown: 30000,
   },
   run: async (client, interaction) => {
-    let embed = new EmbedBuilder();
-    let id = interaction.user.id;
+    try {
+      let embed = new EmbedBuilder();
+      let id = interaction.user.id;
 
-    const now = Date.now();
-    const cooldownTime = 15 * 1000; // 1 minute
-    const lastExecutionTime = cooldownMap.get(interaction.channelId);
-    if (lastExecutionTime && now - lastExecutionTime < cooldownTime) {
-      return; // cooldown not expired, skip execution
+      const now = Date.now();
+      const cooldownTime = 15 * 1000; // 1 minute
+      const lastExecutionTime = cooldownMap.get(interaction.channelId);
+      if (lastExecutionTime && now - lastExecutionTime < cooldownTime) {
+        return; // cooldown not expired, skip execution
+      }
+
+      let members = await getAllMessage(interaction);
+      if (members.length < 1) {
+        await interaction.deferReply({ ephemeral: true });
+        embed.setDescription(
+          `\`#${interaction.channel.name}\`
+            Không có ai để ăn vạ.`
+        );
+        return await interaction.followUp({ embeds: [embed], ephemeral: true });
+      }
+      let profile = await fetchUser(interaction.user.id);
+      if (profile.coin < 36) {
+        await interaction.deferReply({ ephemeral: true });
+        embed.setDescription(
+          `\`#${interaction.channel.name}\`
+            Không đủ tiền để ăn vạ`
+        );
+        return await interaction.followUp({ embeds: [embed], ephemeral: true });
+      }
+
+      const status = await isDebuff(id);
+      if (status) {
+        await interaction.deferReply({ ephemeral: true });
+        embed.setDescription(
+          `\`#${interaction.channel.name}\`
+            Dính trạng thái, không ăn vạ được nhé`
+        );
+        return await interaction.followUp({ embeds: [embed] });
+      }
+      await interaction.deferReply();
+      let result = await anvaProcess(profile, members);
+
+      await interaction.followUp({ content: result });
+
+      await statsInc(id, StatsField.COIN, -36);
+      cooldownMap.set(interaction.channelId, now);
+    } catch (err) {
+      error(err);
     }
-
-    let members = await getAllMessage(interaction);
-    if (members.length < 1) {
-      await interaction.deferReply({ ephemeral: true });
-      embed.setDescription(
-        `\`#${interaction.channel.name}\`
-          Không có ai để ăn vạ.`
-      );
-      return await interaction.followUp({ embeds: [embed], ephemeral: true });
-    }
-    let profile = await fetchUser(interaction.user.id);
-    if (profile.coin < 36) {
-      await interaction.deferReply({ ephemeral: true });
-      embed.setDescription(
-        `\`#${interaction.channel.name}\`
-          Không đủ tiền để ăn vạ`
-      );
-      return await interaction.followUp({ embeds: [embed], ephemeral: true });
-    }
-
-    const status = await isDebuff(id);
-    if (status) {
-      await interaction.deferReply({ ephemeral: true });
-      embed.setDescription(
-        `\`#${interaction.channel.name}\`
-          Dính trạng thái, không ăn vạ được nhé`
-      );
-      return await interaction.followUp({ embeds: [embed] });
-    }
-    await interaction.deferReply();
-    let result = await anvaProcess(profile, members);
-
-    await interaction.followUp({ content: result });
-
-    await statsInc(id, StatsField.COIN, -36);
-    cooldownMap.set(interaction.channelId, now);
   },
 }).toJSON();
